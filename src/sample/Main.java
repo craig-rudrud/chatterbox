3df3556f493c8e3b974c5fddd368f732d98ea72c
Craig Rudrud <rudru017@d.umn.edu>
2018-11-28 13:33:09 -0600

    Designed the user interface.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,10 +12,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setMinWidth(600);
+        primaryStage.setMinHeight(500);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
