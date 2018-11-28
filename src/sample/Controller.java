6de75950421662d93f6505189382527637e5f988
Craig Rudrud <rudru017@d.umn.edu>
2018-11-28 14:48:41 -0600

    Added initializer for Controller class.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button hostButton;
+    @FXML
+    private Button joinButton;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private TextArea chatLogArea;
+    @FXML
+    private TextField messageField;
+    @FXML
+    private TextField receiverIPField;
+    @FXML
+    private TextField receiverPortField;
+    @FXML
+    private TextField senderIPField;
+    @FXML
+    private TextField senderPortField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
