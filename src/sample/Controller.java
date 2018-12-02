7ca282a216f661bf582976ebbf8ae88558a0a436
Craig Rudrud <rudru017@d.umn.edu>
2018-12-01 19:29:34 -0600

    Server and client now send real-time messages to each other.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,7 +8,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
 import java.io.*;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -37,24 +40,25 @@ public class Controller implements Initializable {
         mode = -1;
 
         hostButton.setOnAction(event -> {
-            if(mode != 0) {
+            if (mode != 0) {
                 hostIP = receiverIPField.getText();
                 hostPort = receiverPortField.getText();
                 try {
                     serverSocket = new ServerSocket(Integer.valueOf(hostPort), 0, InetAddress.getByName(hostIP));
                     serverSocket.setSoTimeout(10000);
 
-                    chatLogArea.appendText("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-                    Socket server = serverSocket.accept();
+                    chatLogArea.appendText("Waiting for client on port " + serverSocket.getLocalPort() + "...\n");
+                    server = serverSocket.accept();
 
-                    chatLogArea.appendText("Just connected to " + server.getRemoteSocketAddress());
-                    DataInputStream in = new DataInputStream(server.getInputStream());
+                    chatLogArea.appendText("Just connected to " + server.getRemoteSocketAddress() + "\n");
                     mode = 0;
 
-                    chatLogArea.appendText(in.readUTF());
-                    DataOutputStream out = new DataOutputStream(server.getOutputStream());
-                    out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress() + "\nGoodbye!");
-                    server.close();
+                    out = new DataOutputStream(server.getOutputStream());
+                    in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+
+                    Server s = new Server();
+                    s.start();
+//                    server.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -62,24 +66,22 @@ public class Controller implements Initializable {
         });
 
         joinButton.setOnAction(event -> {
-            if(mode != 1) {
+            if (mode != 1) {
                 hostIP = receiverIPField.getText();
                 hostPort = receiverPortField.getText();
                 try {
                     chatLogArea.appendText("Connecting to " + hostIP + " on port " + hostPort + "...\n");
-                    Socket client = new Socket(InetAddress.getByName(hostIP), Integer.parseInt(hostPort));
+                    client = new Socket(InetAddress.getByName(hostIP), Integer.parseInt(hostPort));
 
-                    chatLogArea.appendText("Just connected to " + client.getRemoteSocketAddress());
-                    OutputStream outToServer = client.getOutputStream();
-                    DataOutputStream out = new DataOutputStream(outToServer);
+                    chatLogArea.appendText("Just connected to " + client.getRemoteSocketAddress() + "\n");
                     mode = 1;
 
-                    out.writeUTF("Hello from " + client.getLocalSocketAddress());
-                    InputStream inFromServer = client.getInputStream();
-                    DataInputStream in = new DataInputStream(inFromServer);
+                    out = new DataOutputStream(client.getOutputStream());
+                    in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-                    chatLogArea.appendText("Server says " + in.readUTF());
-                    client.close();
+                    Client c = new Client();
+                    c.start();
+//                    client.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -95,17 +97,52 @@ public class Controller implements Initializable {
         });
     }
 
-
-
     private void sendMessage() {
-        if(!messageField.getText().isEmpty()) {
-            chatLogArea.appendText(messageField.getText() + "\n");
-            messageField.setText("");
+        String message = messageField.getText();
+        if(!message.isEmpty()) {
+            try {
+                out.writeUTF("Other: " + message + "\n");
+                chatLogArea.appendText("You: " + message + "\n");
+                messageField.setText("");
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    class Server extends Thread {
+        public void run() {
+            String input;
+            try {
+                while ((input = in.readLine()) != null) {
+                    chatLogArea.appendText(input);
+                }
+            } catch (IOException e) {
+                chatLogArea.appendText("Unable to get messages!");
+            }
+        }
+    }
+
+    class Client extends Thread {
+        public void run() {
+            String input;
+            try {
+                while ((input = in.readLine()) != null) {
+                    chatLogArea.appendText(input);
+                }
+            } catch (IOException e) {
+                chatLogArea.appendText("Unable to get messages!");
+            }
+        }
+    }
+
+    private BufferedReader in;
+    private DataOutputStream out;
     private int mode;
     private ServerSocket serverSocket;
+    private Socket client;
+    private Socket server;
     private String clientIP;
     private String clientPort;
     private String hostIP;
